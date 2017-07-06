8a8bdb9d7ddabe7cbc1c7e2af6080bea2665f0e1
PrakashRajendran <prakash.raj@full.co>
2017-07-06 19:16:58 +0530

    Adding user information when add user button clicked

--- war/Javascript/addnewuser.js
+++ war/Javascript/addnewuser.js
@@ -1,16 +1,16 @@
-function userInformation() {
+function addNewUser(){
 	var httpRequest;
 	if (window.XMLHttpRequest) {
 		httpRequest = new XMLHttpRequest();
 	} else {
 		httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
 	}
-	httpRequest.open("GET", "/api/v1/me", true);
+	httpRequest.open("GET", "/adduser", true);
 	httpRequest.onreadystatechange = function() {
 		if (httpRequest.readyState == 4 && httpRequest.status == 200) {
 			var obj = JSON.parse(httpRequest.responseText);
-			document.getElementById("name").innerHTML = obj.name;
+			alert("User added successfully "+obj.name);
 		}
 	}
-	httpRequest.send();
+	httpRequest.send(); 
 }
